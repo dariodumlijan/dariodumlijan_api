465e61a1b4d9c28a6343b6ad476764ee5b092bbb
Dario Dumlijan <dario.dumlijan1@gmail.com>
2022-03-05 00:17:09 +0100

    Ping route added

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def index
+    redirect_to "https://dariodumlijan.com"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'main#index'
   post '/mail', :to => 'mail#send'
 end
